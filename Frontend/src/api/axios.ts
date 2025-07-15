0cc0e19ddea9d4475ade8ee9bc866c22945bd132
CLAIRE NJERI <160070818+merimela-nora@users.noreply.github.com>
2025-07-15 12:18:11 +0300

    Update axios.ts

--- Frontend/src/api/axios.ts
+++ Frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:5454",
+    baseURL: "https://blogit-website-5.onrender.com",
     withCredentials: true
 })
 
